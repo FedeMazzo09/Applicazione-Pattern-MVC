5f1c7214d7b062eee18872e380ca4ad0c0f9abc0
FedeMazzo09 <mazzoleni.federico.studente@itispaleocapa.it>
2026-04-17 09:51:52 +0000

    separazione databse da controller

--- user-manager-mvc/app/controllers/userController.js
+++ user-manager-mvc/app/controllers/userController.js
@@ -1,19 +1,23 @@
 // app/controllers/userController.js
-const User = require('../models/userModel');
+const userRepository = require('../repositories/userRepository');
+
 exports.home = (req, res) => {
   res.render('index');
 };
+
 exports.listUsers = (req, res) => {
-  const users = User.findAll();
+  const users = userRepository.findAll();
   res.render('users', { users });
 };
+
 exports.createUser = (req, res) => {
   const { name, email, linkImg } = req.body;
-  User.create(name, email, linkImg);
+  userRepository.create(name, email, linkImg);
   res.redirect('/users');
 };
+
 exports.deleteUser = (req, res) => {
   const { id } = req.params;
-  User.delete(id);
+  userRepository.delete(id);
   res.redirect('/users');
 };
--- user-manager-mvc/app/models/userModel.js
+++ user-manager-mvc/app/models/userModel.js
@@ -1,46 +1,12 @@
-// app/models/userModel.js
+// app/models/user.js
 const { v4: uuidv4 } = require('uuid');
 
 class User {
-  static users = [];
-
   constructor(name, email, linkImg) {
     this.id = uuidv4();
     this.name = name;
     this.email = email;
     this.linkImg = linkImg;
-  }
-
-  static findAll() {
-    return User.users;
-  }
-
-  static create(name, email, linkImg) {
-    const newUser = new User(name, email, linkImg);
-    User.users.push(newUser);
-    return newUser;
-  }
-
-  static findById(id) {
-    for (let i = 0; i < User.users.length; i++) {
-      if (User.users[i].id === parsedId) {
-        return User.users[i];
-      }
-    }
-
-    return undefined;
-  }
-
-  static delete(id) {
-    let newUsers = [];
-
-    for (let i = 0; i < User.users.length; i++) {
-      if (User.users[i].id !== parsedId) {
-        newUsers.push(User.users[i]);
-      }
-    }
-
-    User.users = newUsers;
   }
 }
 
