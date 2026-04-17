fe08ada44ba40e54f0868689ff439ad81a8756d1
FedeMazzo09 <mazzoleni.federico.studente@itispaleocapa.it>
2026-04-17 09:24:44 +0000

    Trasformazione operazioni funzionali in procedurali

--- user-manager-mvc/app/models/userModel.js
+++ user-manager-mvc/app/models/userModel.js
@@ -1,15 +1,47 @@
 // app/models/userModel.js
-let users = [];
-const User = {
-  findAll: () => users,
-  create: (name, email, linkImg) => {
-    const newUser = { id: Date.now(), name, email, linkImg };
-    users.push(newUser);
-    return newUser;
-  },
-  findById: (id) => users.find(u => u.id === parseInt(id)),
-  delete: (id) => {
-    users = users.filter(u => u.id !== parseInt(id));
+const { v4: uuidv4 } = require('uuid');
+
+class User {
+  static users = [];
+
+  constructor(name, email, linkImg) {
+    this.id = uuidv4();
+    this.name = name;
+    this.email = email;
+    this.linkImg = linkImg;
   }
-};
+
+  static findAll() {
+    return User.users;
+  }
+
+  static create(name, email, linkImg) {
+    const newUser = new User(name, email, linkImg);
+    User.users.push(newUser);
+    return newUser;
+  }
+
+  static findById(id) {
+    for (let i = 0; i < User.users.length; i++) {
+      if (User.users[i].id === parsedId) {
+        return User.users[i];
+      }
+    }
+
+    return undefined;
+  }
+
+  static delete(id) {
+    let newUsers = [];
+
+    for (let i = 0; i < User.users.length; i++) {
+      if (User.users[i].id !== parsedId) {
+        newUsers.push(User.users[i]);
+      }
+    }
+
+    User.users = newUsers;
+  }
+}
+
 module.exports = User;
